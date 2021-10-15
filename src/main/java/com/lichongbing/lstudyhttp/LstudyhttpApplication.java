94248c3892ad3116284e8a3a030706558b26f7a9
lichongbing <865143152@qq.com>
2021-10-15 21:33:18 +0800

    Initial commit

---
+++ src/main/java/com/lichongbing/lstudyhttp/LstudyhttpApplication.java
@@ -0,0 +1,13 @@
+package com.lichongbing.lstudyhttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LstudyhttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LstudyhttpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lichongbing/lstudyhttp/LstudyhttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lichongbing.lstudyhttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LstudyhttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
